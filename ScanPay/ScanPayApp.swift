647d6de908ce0748f702a59b2ab0b81acc89059f
Adam Zhao <adamzhao@stanford.edu>
2024-04-22 13:05:47 -0700

    Initial Commit

---
+++ ScanPay/ScanPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanPayApp.swift
+//  ScanPay
+//
+//  Created by Adam Zhao on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ScanPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
